eca75259b53e3b15288939fb630cbc8fe3565f67
GnoxNahte <gnoxnahte.dev@gmail.com>
2026-01-29 23:52:21 +0800

    Fix ImGui input and organise vs filter

--- AlphaEngine_BaseProject/Source/Main.cpp
+++ AlphaEngine_BaseProject/Source/Main.cpp
@@ -6,13 +6,15 @@
 #include "AEEngine.h"
 #include "Game/Scene/GSM.h"
 
-#include <windows.h>
-#include <GL/gl.h>
+#include <iostream>
 
+#include <windows.h>
 #include <imgui.h>
 #include <imgui_impl_win32.h>
 #include <imgui_impl_opengl3.h>
-//extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 // ---------------------------------------------------------------------------
 // main
 
@@ -29,7 +31,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	// ===== Setup window and AlphaEngine =====
 
 	// Using custom window procedure
-	AESysInit(hInstance, nCmdShow, 1600, 900, 1, 240, false, NULL);
+	AESysInit(hInstance, nCmdShow, 1600, 900, 1, 240, false, WndProc);
 	
 	// Changing the window title
 	AESysSetWindowTitle("GAM 150"); // @todo: change name
@@ -54,13 +56,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	//ImGui::StyleColorsDark();
 
-	//// Setup scaling
-	//float main_scale = ImGui_ImplWin32_GetDpiScaleForMonitor(::MonitorFromPoint(POINT{ 0, 0 }, MONITOR_DEFAULTTOPRIMARY));
-	//ImGuiStyle& style = ImGui::GetStyle();
-	//style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
-	//style.FontScaleDpi = main_scale;        // Set initial font scale. (in docking branch: using io.ConfigDpiScaleFonts=true automatically overrides this for every window depending on the current monitor)
-	//io.ConfigDpiScaleFonts = true;          // [Experimental] Automatically overwrite style.FontScaleDpi in Begin() when Monitor DPI changes. This will scale fonts but _NOT_ scale sizes/padding for now.
-	//io.ConfigDpiScaleViewports = true;      // [Experimental] Scale Dear ImGui and Platform Windows when Monitor DPI changes.
+	// Setup scaling
+	float main_scale = ImGui_ImplWin32_GetDpiScaleForMonitor(::MonitorFromPoint(POINT{ 0, 0 }, MONITOR_DEFAULTTOPRIMARY));
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+	style.FontScaleDpi = main_scale;        // Set initial font scale. (in docking branch: using io.ConfigDpiScaleFonts=true automatically overrides this for every window depending on the current monitor)
+	io.ConfigDpiScaleFonts = true;          // [Experimental] Automatically overwrite style.FontScaleDpi in Begin() when Monitor DPI changes. This will scale fonts but _NOT_ scale sizes/padding for now.
+	io.ConfigDpiScaleViewports = true;      // [Experimental] Scale Dear ImGui and Platform Windows when Monitor DPI changes.
 
 	//// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
 	//if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
@@ -103,4 +105,39 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	// free the system
 	AESysExit();
+}
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Win32 message handler
+// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
+
+	//std::cout << std::hex << "MSG: " << msg << "\n";
+	switch (msg)
+	{
+	case WM_SIZE:
+		/*if (wParam != SIZE_MINIMIZED)
+		{
+			g_Width = LOWORD(lParam);
+			g_Height = HIWORD(lParam);
+		}*/
+
+		return 0;
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+			return 0;
+		break;
+	case WM_DESTROY:
+		GSM::ChangeScene(SceneState::GS_QUIT);
+		::PostQuitMessage(0);
+		return 0;
+	}
+	return ::DefWindowProcW(hWnd, msg, wParam, lParam);
 }
