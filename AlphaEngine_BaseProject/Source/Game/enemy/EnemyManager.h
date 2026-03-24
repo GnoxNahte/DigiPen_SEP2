16e31233bbc06d205b00ceaf60636e700197fdd1
kang890 <limkangping99@gmail.com>
2026-03-24 21:31:55 +0800

    Update EnemyManager.h

--- AlphaEngine_BaseProject/Source/Game/enemy/EnemyManager.h
+++ AlphaEngine_BaseProject/Source/Game/enemy/EnemyManager.h
@@ -170,7 +170,7 @@ public:
                 if (currentRoomId != ROOM_NONE)
                     depth = static_cast<int>(currentRoomId) - static_cast<int>(ROOM_1);
 
-                e->ApplyRoomScaling(depth * 10, depth * 1);
+                e->ApplyRoomScaling(depth * 2, depth);
                 enemies.emplace_back(std::move(e));
             }
             break;
