c29ebd88617dc6adf00372d73cfeecff5ca45d58
kang890 <limkangping99@gmail.com>
2026-03-24 23:33:45 +0800

    Update Player.cpp

--- AlphaEngine_BaseProject/Source/Game/Player/Player.cpp
+++ AlphaEngine_BaseProject/Source/Game/Player/Player.cpp
@@ -353,7 +353,7 @@ void Player::HandleLanding()
         emitter.angleRange.x = AEDegToRad(180.f);
         emitter.angleRange.y = AEDegToRad(180.f - angleRange);
         particleSystem.SpawnParticleBurst(emitter, spawnCount);
-        Camera::StartShake(0.12f, 0.15f);
+        Camera::StartShake(0.25f, 0.25f);
     }
     // @todo: (Ethan) - Play sound
 }
