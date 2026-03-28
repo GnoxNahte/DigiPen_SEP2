8dea8a2b3f6a670e18122410dbb6131ab664697c
kang890 <limkangping99@gmail.com>
2026-03-29 00:27:34 +0800

    adjust gamescene update

--- AlphaEngine_BaseProject/Source/Game/enemy/EnemyBoss.cpp
+++ AlphaEngine_BaseProject/Source/Game/enemy/EnemyBoss.cpp
@@ -496,9 +496,6 @@ void EnemyBoss::Update(const AEVec2& playerPos, bool playerFacingRight, MapGrid&
 	if (hpBarShown < 0.f) hpBarShown = 0.f;
 	if (hpBarShown > 1.5) hpBarShown = 1.5f;
 
-
-    
-
   if (isDead)
     {
         // Ensure we are in DEATH state (safe to call; SetState ignores same-state)
