918017b602146728cdc599eec056a678a76f034b
kang890 <limkangping99@gmail.com>
2026-04-04 01:01:29 +0800

    add file reading for enemy and boss
    boss file reading is honestly redundant, just to improve marking/rubrics?

---
+++ AlphaEngine_BaseProject/Source/Game/enemy/EnemyBossStats.cpp
@@ -0,0 +1,57 @@
+#include <rapidjson/document.h>
+#include <iostream>
+#include <string>
+
+#include "EnemyBossStats.h"
+#include "../../Utils/FileHelper.h"
+
+EnemyBossStats::EnemyBossStats(std::string file)
+    : file(file)
+{
+    LoadFileData();
+}
+
+void EnemyBossStats::LoadFileData()
+{
+    rapidjson::Document doc;
+    bool success = FileHelper::TryReadJsonFile(file, doc);
+
+    if (!success)
+    {
+        std::cout << "[EnemyBossStats] Failed to read file: " << file << '\n';
+        return;
+    }
+
+    maxHP = doc["maxHP"].GetInt();
+    attackDamage = doc["attackDamage"].GetInt();
+
+    moveSpeed = doc["moveSpeed"].GetFloat();
+    aggroRange = doc["aggroRange"].GetFloat();
+    aggroYRange = doc["aggroYRange"].GetFloat();
+    attackYRange = doc["attackYRange"].GetFloat();
+
+    phase2HpThreshold = doc["phase2HpThreshold"].GetFloat();
+
+    sizeX = doc["size"]["x"].GetFloat();
+    sizeY = doc["size"]["y"].GetFloat();
+
+    attackStartRange = doc["attack"]["startRange"].GetFloat();
+    attackHitRange = doc["attack"]["hitRange"].GetFloat();
+    attackCooldown = doc["attack"]["cooldown"].GetFloat();
+    attackHitTimeNormalized = doc["attack"]["hitTimeNormalized"].GetFloat();
+    attackBreakRange = doc["attack"]["breakRange"].GetFloat();
+
+    meleeHitboxWidth = doc["meleeHitbox"]["width"].GetFloat();
+    meleeHitboxHeight = doc["meleeHitbox"]["height"].GetFloat();
+    meleeHitboxXInset = doc["meleeHitbox"]["xInset"].GetFloat();
+    meleeHitboxYOffset = doc["meleeHitbox"]["yOffset"].GetFloat();
+
+    teleportInterval = doc["teleport"]["interval"].GetFloat();
+    teleportBehindOffset = doc["teleport"]["behindOffset"].GetFloat();
+    teleportHalfRange = doc["teleport"]["halfRange"].GetFloat();
+    teleportWallPadding = doc["teleport"]["wallPadding"].GetFloat();
+    teleportMinPlayerGap = doc["teleport"]["minPlayerGap"].GetFloat();
+
+    minHurtDuration = doc["hurt"]["minHurtDuration"].GetFloat();
+    staggerResetDelay = doc["hurt"]["staggerResetDelay"].GetFloat();
+}
